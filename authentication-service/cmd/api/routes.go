381c2c657be1afd89bf77d82967cf4882dd60444
Mateusz Dyrda <mateusz.dyrda@cloud.ionos.com>
2025-04-29 09:42:06 +0200

    first commit

---
+++ authentication-service/cmd/api/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"net/http"
+)
+
+func (app *Config) routes() http.Handler {
+
+	mux := chi.NewRouter()
+
+	// specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-TOKEN"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	return mux
+}
